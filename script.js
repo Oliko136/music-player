415a84048fed1382384e6593bea2dc0717a21f6c
Oliko136 <olchick136@gmail.com>
2025-09-13 13:37:46 +0400

    Play previous and next songs

--- script.js
+++ script.js
@@ -38,6 +38,30 @@ function pauseSong() {
     audio.pause();
 }
 
+// Play previous song
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Play next song
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
 
@@ -47,3 +71,6 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
